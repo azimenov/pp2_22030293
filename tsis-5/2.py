250fd4f86f0ad5fac0bc3d14aaee958f885d43a1
azimenov <113203599+azimenov@users.noreply.github.com>
2023-03-07 10:12:30 +0300

    Update 2.py

--- tsis-5/2.py
+++ tsis-5/2.py
@@ -1,8 +1,8 @@
 import re
-def task2(text):
-    pattern = 'ab{2,3}'
-    x = re.findall(pattern, text)
-    try:
-        print(x)
-    except AttributeError as e:
-        print("Not found")
+text_to_search = input()
+
+pattern = re.compile(r'ab{2,3}')
+matches = pattern.finditer(text_to_search)
+
+for match in matches:
+    print(match)
